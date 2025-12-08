0580c9303be27cf871e1cf876f357b149ff18b53
Niroj Magar <youkuikunda@gmail.com>
2025-12-08 12:52:39 +0545

    deploy

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -149,8 +149,8 @@ export default function Login() {
             <div className="mt-6 p-4 bg-secondary rounded-lg">
               <p className="text-xs text-muted-foreground text-center">
                 Use your BMT credentials to sign in.
-                <br />
-                Admin: admin@bmtops.com
+                {/* <br /> */}
+                {/* Admin: admin@bmtops.com */}
               </p>
             </div>
           </div>
